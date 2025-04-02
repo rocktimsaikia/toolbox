f5c0387d205ecf4ae5166f5d98902ce6f0323d4d
Rocktim Saikia <rocktimthedev@gmail.com>
2025-04-02 19:34:37 +0000

    Fix sitemap issue

--- src/app/sitemap.ts
+++ src/app/sitemap.ts
@@ -1,23 +1,22 @@
+import { SLUGS } from "@/constants/tools";
 import type { MetadataRoute } from "next";
-import {SLUGS} from "./constants/tools";
 
 const BASE_URL = "https://tools.rocktimsaikia.dev";
 
 const tools = SLUGS.map((slug) => ({
-    url: `${BASE_URL}/${slug}`,
-    lastModified: new Date(),
-    changeFrequency: "monthly",
-    priority: 1,
-})
+  url: `${BASE_URL}/${slug}`,
+  lastModified: new Date(),
+  changeFrequency: "monthly" as const,
+  priority: 0.8,
+}));
 
-export default function sitemap(): MetadataRoute.Sitemap {
+export default function sitemap() {
   return [
     {
       url: BASE_URL,
       lastModified: new Date(),
-      changeFrequency: "yearly",
+      changeFrequency: "yearly" as const,
       priority: 1,
     },
-    ...tools
   ];
 }
