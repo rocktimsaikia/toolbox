132caa6ed8ac0e758943dc529a3afe186bf318c5
Rocktim <rocktimthedev@gmail.com>
2025-05-16 12:27:40 +0530

    Add html-escape tool (#2)
    * Add html-escape tool
    
    * Update main page meta data
    
    * Update pnpm lock file

---
+++ src/app/html-escape/layout.tsx
@@ -0,0 +1,23 @@
+import Faq from "@/components/faq";
+import { Faqs } from "@/constants/faq";
+import { TOOLS } from "@/constants/tools";
+import type { Metadata } from "next";
+
+const slug = "html-escape";
+
+const tool = TOOLS[slug];
+const faq = Faqs[slug];
+
+export const metadata: Metadata = {
+  title: tool.name,
+  description: tool.description,
+};
+
+export default function Layout({ children }: Readonly<{ children: React.ReactNode }>) {
+  return (
+    <div>
+      {children}
+      <Faq faq={faq} />
+    </div>
+  );
+}
